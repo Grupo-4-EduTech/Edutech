d568d4bae3ead004b538db6a447e0b0e4e940de4
Murakami-J <juliana.oshikawa@sptech.school>
2024-11-01 19:15:45 -0300

    Ajuste

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -21,17 +21,6 @@ function autenticar(req, res) {
                         console.log(resultadoAutenticar);
                             res.status(200).json(resultadoAutenticar);
                         
-                            then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
-                                    });
-                                } 
-                            })
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
