66d3f26a2c51df547a232b5c6b2c44e64feae18e
Murakami-J <juliana.oshikawa@sptech.school>
2024-12-01 01:13:50 -0300

    Implementação do slack

---
+++ Java/src/main/java/Slack.java
@@ -0,0 +1,28 @@
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author Diego Brito <diego.lima@bandtec.com.br>
+ */
+public class Slack {
+
+    private static final HttpClient client = HttpClient.newHttpClient();
+    private static final String URL = "https://hooks.slack.com/services/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";
+
+    public static void sendMessage(JSONObject content) throws IOException, InterruptedException {
+
+        HttpRequest request = HttpRequest.newBuilder(
+                        URI.create(URL))
+                .header("accept", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(content.toString()))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+}
